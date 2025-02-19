d89fcd021751e3e950e3271d46eb7ce81a2e9ae9
Kris <krisriva86@gmail.com>
2025-02-19 18:16:24 +0300

    Work on mixin selectDropdown.

--- src/assets/favicons/favicons.js
+++ src/assets/favicons/favicons.js
@@ -1,14 +1,7 @@
-// const faviconsContext = require.context(
-//   './', // Указывает на папку favicons
-//   true, // Рекурсивно
-//   /\.(svg|png|ico|xml|json)$/ // Поддерживаемые форматы
-// );
-
-// faviconsContext.keys().forEach(faviconsContext);
-
 const faviconsContext = require.context( 
   '!!file-loader?name=assets/favicons/[name].[ext]!.', 
   true, 
   /\.(svg|png|ico|xml|json)$/ 
 );
+
 faviconsContext.keys().forEach(faviconsContext);
